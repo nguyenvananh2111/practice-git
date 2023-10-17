8db0f90377e10c141c99c1cdf8f5e62663ea1420
nguyenvananh2111 <nguyenvananh2111@gmail.com>
2023-10-17 22:07:20 +0700

    aaa

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const getFile = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             resolve("data 33333");
+            console.log("aaaaa");
         }, 1000);
     });
 };
