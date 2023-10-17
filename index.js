63ae65bf3f2b8b78afad5b9493af30cc4f83647a
nguyenvananh2111 <nguyenvananh2111@gmail.com>
2023-10-17 21:50:51 +0700

    oh my god

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 const getFile = () => {
     return new Promise((resolve, reject) => {
         setTimeout(() => {
-            console.log('bbb');
             resolve("data 33333");
-            console.log('ccc');
         }, 1000);
     });
 };
